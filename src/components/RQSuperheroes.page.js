092643be428858204757821093914c618b0a9b8d
John Pathadan <john.paul@vonnue.com>
2023-11-25 14:18:53 +0530

    feat: initial setup

---
+++ src/components/RQSuperheroes.page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RQSuperheroesPage = () => {
+  return <div>RQ Super heroes Page</div>;
+};
+
+export default RQSuperheroesPage;
---
+++ src/components/SuperHeroes.page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuperHeroesPage = () => {
+  return <div>Super Heroes Page</div>;
+};
+
+export default SuperHeroesPage;
